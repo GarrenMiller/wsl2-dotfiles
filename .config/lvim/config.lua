2bbd0508858ba79875ba8229d3114f06767235c4
Garren Miller <gmiller@unwiredrevolution.com>
2022-07-31 15:36:48 -0700

    Forgot to default colorscheme to kanagawa

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -11,7 +11,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.colorscheme = "tokyonight"
+lvim.colorscheme = "kanagawa"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
